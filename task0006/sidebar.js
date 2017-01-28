cdfd119291b105230a583496c158566822805eb5
00Z26 <lwlau@qq.com>
2017-01-28 23:21:00 +0800

    刘宛玲 第六次作业

---
+++ task0006/sidebar.js
@@ -0,0 +1,32 @@
+/**
+ * Created by lenovo on 2017/1/27.
+ */
+(function( ){
+var Sidebar = function(eID,closebarID ){
+    this.state = 'open';
+    this.el = document.getElementById(eID||'sidebar');
+    this.closeBarEl = document.getElementById(closebarID||'closebar');
+    var self = this;
+    this.el.addEventListener('click',function(event){});
+        if( event.target !==this.el){
+            self.triggerSwitch();
+        }
+};
+    Sidebar.prototype.close = function(){
+    console.log('关闭sidebar');
+        this.state='closed';
+    };
+    Sidebar.prototype.open = function(){
+        console.log('opensidebar')
+        this.state='opened';
+    };
+    Sidebar.prototype.triggerSwitch = function(){
+        if(this.state === 'opened'){
+            this.close();
+        }
+        else{
+            this.open();
+        }
+    };
+    var sidebar = new Sidebar();
+})();
